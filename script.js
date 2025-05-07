b0debaace040e868e8f4fe46afd4b52acbf15eb4
anshul-dhomne <anshul.dhomnee@gmail.com>
2025-05-07 12:02:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function firstChar(text) {
-  // your code here
+function firstChar(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      return str[i];
+    }
+  }
+  return '';
 }
+
+// Example usage:
+console.log(firstChar(' Rosa Parks '));   // Output: 'R'
+console.log(firstChar(' Hello World '));  // Output: 'H'
+console.log(firstChar(' '));              // Output: ''
+console.log(firstChar(''));               // Output: ''
+
+
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
